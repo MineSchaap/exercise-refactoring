2c7fd628a3daa77b93691a1bb51ca0cb8d524be3
Meron Brouwer <meron.brouwer@gmail.com>
2019-08-18 12:12:20 +0200

    Refactored the Unittest to comply with the DoD

--- src/test/java/nl/han/ica/oose/dea/OrdersWriterTest.java
+++ src/test/java/nl/han/ica/oose/dea/OrdersWriterTest.java
@@ -6,49 +6,90 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-public class OrdersWriterTest {
-    Orders orders = new Orders();
-    Order order111 = new Order(111);
+class OrdersWriterTest {
+
+    private Orders orders;
+    private Order order111;
 
     @BeforeEach
-    public void SetupOneOrder() {
+    void setup() {
+        orders = new Orders();
+        order111 = new Order(111);
+
         orders.AddOrder(order111);
     }
 
     @Test
-    public void NoOrder() {
-        assertEquals("{\"orders\": []}", new OrdersWriter(new Orders()).getContents());
+    void emptyOrdersReturnsCorrectJsonString() {
+        // Arrange
+        var ordersWriter = new OrdersWriter(new Orders());
+
+        // Act
+        var ordersContent = ordersWriter.getContents();
+
+        // Assert
+        assertEquals("{\"orders\": []}", ordersContent);
     }
 
     @Test
-    public void OneOrder() {
-        String order111 = "{\"id\": 111, \"products\": []}";
-        assertEquals("{\"orders\": [" + order111 + "]}", new OrdersWriter(orders).getContents());
+    void oneOrderReturnsCorrectJsonString() {
+        // Arrange
+        var order111 = "{\"id\": 111, \"products\": []}";
+        var ordersWriter = new OrdersWriter(orders);
+
+        // Act
+        var ordersContent = ordersWriter.getContents();
+
+        // Assert
+        assertEquals("{\"orders\": [" + order111 + "]}", ordersContent);
     }
 
     @Test
-    public void TwoOrders() {
+    void twoOrdersReturnsCorrectJsonString() {
+        // Arrange
         orders.AddOrder(new Order(222));
+        var ordersWriter = new OrdersWriter(orders);
 
-        String order111Json = JsonOrder111WithProduct("");
-        String order222Json = "{\"id\": 222, \"products\": []}";
-        assertEquals("{\"orders\": [" + order111Json + ", " + order222Json + "]}", new OrdersWriter(orders).getContents());
+        var order111Json = JsonOrder111WithProduct("");
+        var order222Json = "{\"id\": 222, \"products\": []}";
+
+        // Act
+        var ordersContent = ordersWriter.getContents();
+
+        // Assert
+        assertEquals("{\"orders\": [" + order111Json + ", " + order222Json + "]}", ordersContent);
     }
 
     @Test
-    public void OneOrderWithOneProduct() {
+    void oneOrderWithOneProductReturnsCorrectJsonString() {
+        // Arrange
         order111.AddProduct(new Product("Shirt", 1, 3, 2.99, "TWD"));
 
-        String order111Json = JsonOrder111WithProduct("{\"code\": \"Shirt\", \"color\": \"blue\", \"size\": \"M\", \"price\": 2.99, \"currency\": \"TWD\"}");
-        assertEquals("{\"orders\": [" + order111Json + "]}", new OrdersWriter(orders).getContents());
+        var ordersWriter = new OrdersWriter(orders);
+
+        var order111Json = JsonOrder111WithProduct("{\"code\": \"Shirt\", \"color\": \"blue\", \"size\": \"M\", \"price\": 2.99, \"currency\": \"TWD\"}");
+
+        // Act
+        var ordersContent = ordersWriter.getContents();
+
+        // Assert
+        assertEquals("{\"orders\": [" + order111Json + "]}", ordersContent);
     }
 
     @Test
-    public void OneOrderWithOneProductNoSize() {
+    void oneOrderWithOneProductNoSizeReturnsCorrectJsonString() {
+        // Arrange
         order111.AddProduct(new Product("Pot", 2, -1, 16.50, "SGD"));
 
-        String order111Json = JsonOrder111WithProduct("{\"code\": \"Pot\", \"color\": \"red\", \"price\": 16.5, \"currency\": \"SGD\"}");
-        assertEquals("{\"orders\": [" + order111Json + "]}", new OrdersWriter(orders).getContents());
+        var ordersWriter = new OrdersWriter(orders);
+
+        var order111Json = JsonOrder111WithProduct("{\"code\": \"Pot\", \"color\": \"red\", \"price\": 16.5, \"currency\": \"SGD\"}");
+
+        // Act
+        var ordersContent = ordersWriter.getContents();
+
+        // Assert
+        assertEquals("{\"orders\": [" + order111Json + "]}", ordersContent);
     }
 
     private String JsonOrder111WithProduct(String productJson) {
